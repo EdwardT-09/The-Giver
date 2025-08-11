9ce0ee3d6f22908848181ad62ade1d3193940ade
EdwardT-09 <edwardthong0531@gmail.com>
2025-08-11 17:26:19 +0800

    Update ChangeEmailController.scala with compareEmail

--- src/main/scala-3/donatesystem/view/ChangeEmailController.scala
+++ src/main/scala-3/donatesystem/view/ChangeEmailController.scala
@@ -1,16 +1,28 @@
 package donatesystem.view
 
 
-import donatesystem.util.PatternMatch
+import donatesystem.util.{Alert, PatternMatch, Session}
 import javafx.fxml.FXML
 import javafx.scene.control.TextField
 import scalafx.Includes.*
-import donatesystem.util.Alert
 
 @FXML
 class ChangeEmailController:
   @FXML private var currentEmailField: TextField = _
   @FXML private var newEmailField: TextField = _
+
+  def compareEmail: Boolean =
+    var errorMessage = ""
+    val currentEmail = Session.getAdmin.get.emailProperty
+    if (currentEmail.value != currentEmailField.text.value) then
+      errorMessage += "Current email provided do not match your current email associated with the Giver."
+    end if
+    if (errorMessage.length() == 0) then
+      true
+    else
+      Alert.displayAlert("Invalid email", errorMessage, "Please try again")
+      false
+  end compareEmail
 
   def validEmail(): Boolean =
     var errorMessage: String = ""
@@ -25,7 +37,6 @@ class ChangeEmailController:
     else
       Alert.displayAlert("Invalid Email", errorMessage, "Please try again")
       false
-  
   end validEmail
 
 end ChangeEmailController
