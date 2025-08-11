0672281eb087bb3142c149d073cd721fda8480f4
EdwardT-09 <edwardthong0531@gmail.com>
2025-08-11 22:19:04 +0800

    Update ChangeEmailController and ChangePasswordController

--- src/main/scala-3/donatesystem/view/ChangeEmailController.scala
+++ src/main/scala-3/donatesystem/view/ChangeEmailController.scala
@@ -15,20 +15,34 @@ class ChangeEmailController:
 
 
   def handleChangeEmail(action:ActionEvent): Unit =
-    if (validEmail()) then
-      if (compareEmail) then
+    if (!isNull) then
+      if (validEmail && compareEmail) then
         val admin = new Administrator(0, Session.getAdmin.get.fNameProperty.value, newEmailField.text.value, Session.getAdmin.get.passwordProperty.value)
         admin.saveAsRecord match
           case Success(x) => Alert.displayAlert("Success", "Success", "The email has been updated")
           case Failure(error) => Alert.displayAlert("Unsuccessful", "Email is in use", error.getMessage)
   end handleChangeEmail
 
+  def isNull:Boolean =
+    var errorMessage = ""
+    if(currentEmailField.text.value.isEmpty) then
+      errorMessage += "Current email field is empty\n"
+    end if 
+    if(newEmailField.text.value.isEmpty) then 
+      errorMessage += "New email field is empty\n"
+    end if 
+    if(errorMessage.isEmpty) then 
+      false
+    else 
+      Alert.displayAlert("Field Empty", errorMessage, "Please try again")
+      true
+  end isNull
 
   def compareEmail: Boolean =
     var errorMessage = ""
     val currentEmail = Session.getAdmin.get.emailProperty
     if (currentEmail.value != currentEmailField.text.value) then
-      errorMessage += "Current email provided do not match your current email associated with the Giver."
+      errorMessage += "Current email provided do not match your current email associated with the Giver.\n"
     end if
     if (errorMessage.length() == 0) then
       true
@@ -37,13 +51,13 @@ class ChangeEmailController:
       false
   end compareEmail
 
-  def validEmail(): Boolean =
+  def validEmail: Boolean =
     var errorMessage: String = ""
     if (!PatternMatch.validEmail(currentEmailField.text.value)) then
-      errorMessage += "The current email field does not follow the following format: john@example.com"
+      errorMessage += "The current email field does not follow the following format: john@example.com\n"
     end if
     if (!PatternMatch.validEmail(newEmailField.text.value)) then
-      errorMessage += "The new email field does not follow the following format: john@example.com"
+      errorMessage += "The new email field does not follow the following format: john@example.com\n"
     end if
     if (errorMessage.length() == 0) then
       true
--- src/main/scala-3/donatesystem/view/ChangePasswordController.scala
+++ src/main/scala-3/donatesystem/view/ChangePasswordController.scala
@@ -1,5 +1,53 @@
 package donatesystem.view
 
-class ChangePasswordController
+import donatesystem.util.PatternMatch
+import javafx.fxml.FXML
+import javafx.scene.control.PasswordField
+import scalafx.Includes.*
+import donatesystem.util.Alert
+
+@FXML
+class ChangePasswordController:
+  @FXML private var currentPasswordField: PasswordField = _
+  @FXML private var newPasswordField: PasswordField = _
+  @FXML private var newPasswordConfirmField: PasswordField = _
+
+  def isNull: Boolean =
+    var errorMessage = ""
+    if (currentPasswordField.text.value.isEmpty) then
+      errorMessage += "Current password field is empty\n"
+    end if
+    if (newPasswordField.text.value.isEmpty) then
+      errorMessage += "New password field is empty\n"
+    end if
+    if (newPasswordConfirmField.text.value.isEmpty) then
+      errorMessage += "New password confirmation field is empty\n"
+    end if
+    if (errorMessage.isEmpty) then
+      true
+    else
+      Alert.displayAlert("Field Empty", errorMessage, "Please try again")
+      false
+  end isNull
+
+  def validPassword(): Boolean =
+    var errorMessage: String = ""
+    if (!PatternMatch.validPassword(currentPasswordField.text.value)) then
+      errorMessage += "The current password field must contain at least one upper case, lower case, number and symbol"
+    end if
+    if (!PatternMatch.validEmail(newPasswordField.text.value)) then
+      errorMessage += "The new password field must contain at least one upper case, lower case, number and symbol"
+    end if
+    if(newPasswordField.text.value != newPasswordConfirmField.text.value) then
+      errorMessage += "The password confirmation does not match the new password "
+    end if
+    if (errorMessage.length() == 0) then
+      true
+    else
+      Alert.displayAlert("Invalid Password", errorMessage, "Please try again")
+      false
+  end validPassword
+
+end ChangePasswordController
 
 
